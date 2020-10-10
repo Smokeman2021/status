99b505e871c3c37a92971f72556cfb57df743c32
Smokeman2021 <72651184+Smokeman2021@users.noreply.github.com>
2020-10-10 13:00:17 +0300

    Add files via upload

---
+++ precache-manifest.12a6eba2454801b7c229d793cdd61feb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f470abe35a025c50df4b09b8995f52f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c78bfd470aec0607136a",
+    "url": "/static/css/home-page.b9d9afa3.chunk.css"
+  },
+  {
+    "revision": "01fb78c27127eed95564",
+    "url": "/static/css/options-page.2df91dd5.chunk.css"
+  },
+  {
+    "revision": "15ca21079f8f81553ee0",
+    "url": "/static/js/4.e9166588.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.e9166588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e317f9c0024d88c148",
+    "url": "/static/js/5.ca5b8a8c.chunk.js"
+  },
+  {
+    "revision": "c78bfd470aec0607136a",
+    "url": "/static/js/home-page.772eba25.chunk.js"
+  },
+  {
+    "revision": "9ca6340e0a4acdca192d",
+    "url": "/static/js/main.4cc00cd5.chunk.js"
+  },
+  {
+    "revision": "01fb78c27127eed95564",
+    "url": "/static/js/options-page.69960983.chunk.js"
+  },
+  {
+    "revision": "a65021bea7ef62eaebc1",
+    "url": "/static/js/runtime-main.d2200b12.js"
+  },
+  {
+    "revision": "9fc66bf8af070b8b822edebf3d05cff1",
+    "url": "/static/media/dvig.9fc66bf8.jpg"
+  },
+  {
+    "revision": "64ae3bfea4036c55b924b08d66533156",
+    "url": "/static/media/gbc.64ae3bfe.jpg"
+  },
+  {
+    "revision": "62885464ddf39b60c8fdaed6f78a2c2a",
+    "url": "/static/media/gbo.62885464.jpg"
+  },
+  {
+    "revision": "66ca57464d7dc6dee41e431705da952d",
+    "url": "/static/media/hod.66ca5746.jpg"
+  },
+  {
+    "revision": "26461091d5be45a8700803b30a50f06b",
+    "url": "/static/media/logo.26461091.jpg"
+  },
+  {
+    "revision": "94d9b0b786b76a5dd07b2ba58e6e96eb",
+    "url": "/static/media/main.94d9b0b7.jpg"
+  },
+  {
+    "revision": "d5090a5568c3e02d9d6ad3a932bf8a62",
+    "url": "/static/media/tires.d5090a55.jpg"
+  },
+  {
+    "revision": "d5db01e449b05d0f4fde9756a9d7747b",
+    "url": "/static/media/tnvd.d5db01e4.jpg"
+  },
+  {
+    "revision": "9552b8450256d8325e07d57cb4f68122",
+    "url": "/static/media/turbo.9552b845.jpg"
+  }
+]);
